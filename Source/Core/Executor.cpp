167857ca66dd4290b11cc1324fd4d7ac95cc4862
raygardnercpp <samuelalonso26@icloud.com>
2023-12-24 19:59:04 +0000

    Core Executor

---
+++ Source/Core/Executor.cpp
@@ -0,0 +1,18 @@
+#include "Executor.hpp"
+#include "../Spell/TranslationUnitDecl.hpp"
+#include <iostream>
+
+namespace Selka::Executor
+{
+    void Process(std::string& content)
+    {
+        const nlohmann::json data = nlohmann::json::parse(content);
+        if(not data.is_object())
+        {
+            std::cerr << "The following JSON content ain't a valid JSON object:\n\n" << content;
+            std::abort();
+        }
+        content.clear();
+        data.get<Spell::TranslationUnitDecl>().Shade(content);
+    }
+}
---
+++ Source/Core/Executor.hpp
@@ -0,0 +1,7 @@
+#pragma once
+#include <string>
+
+namespace Selka::Executor
+{
+    void Process(std::string&);
+}
