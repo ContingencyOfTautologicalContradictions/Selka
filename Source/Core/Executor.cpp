af565a8d91be5883656a0bb85897a8be382fef67
raygardnercpp <samuelalonso26@icloud.com>
2023-12-24 21:10:39 +0000

    Core Executor

--- Source/Core/Executor.cpp
+++ Source/Core/Executor.cpp
@@ -13,7 +13,32 @@ namespace Selka::Executor
             ":\n\n" << content;
             std::abort();
         }
-        content.clear();
-        data.get<Spell::TranslationUnitDecl>().Shade(content);
+        else if(data.contains("kind"))
+        {
+            const nlohmann::json& kind = data.at("kind");
+            if(kind.is_string())
+            {
+                if(kind.get<std::string>() == "TranslationUnitDecl")
+                    data.get<Spell::TranslationUnitDecl>().Shade(content);
+                else
+                {
+                    std::cerr << "The following JSON content ain't a valid "
+                    "JSON object:\n\n" << content;
+                    std::abort();
+                }
+            }
+            else
+            {
+                std::cerr << "The following JSON content ain't a valid JSON "
+                "object:\n\n" << content;
+                std::abort();
+            }
+        }
+        else
+        {
+            std::cerr << "The following JSON content ain't a valid JSON object"
+            ":\n\n" << content;
+            std::abort();
+        }
     }
 }
