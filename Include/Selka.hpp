1ca1171f63701fbf19cbe75b6f6f753557195376
raygardnercpp <samuelalonso26@icloud.com>
2023-12-26 18:46:08 +0000

    Selka.hpp

--- Include/Selka.hpp
+++ Include/Selka.hpp
@@ -1,5 +1,5 @@
 #pragma once
-#define compute(x, y, z) static_assert(true, "selka.shading.compute " #x ':' #y ':' #z);
+#define compute(x, y, z) static_assert(true, "selka.shading.compute " #x ":" #y ":" #z);
 #define vertex static_assert(true, "selka.shading.vertex");
 #define fragment static_assert(true, "selka.shading.fragment");
 
